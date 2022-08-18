3e888382422ddd46d46f0e6f6ed9a562b3c1fbb9
Simon <simonchauvel@gmail.com>
2022-08-18 16:11:33 +0200

    add zombie's controls

--- js/zombie.js
+++ js/zombie.js
@@ -1,22 +1,43 @@
 'use strict';
 
 const zombie = {
-    image         :     "img/zombie.png",
-    domObj        :     null,
-    spriteSize    :     100,
-    numberSprite  :     10
+    image           :       "img/zombie.png",
+    domObj          :       null,
+    spriteSize      :       100,
+    numberSprite    :       10,
+    direction       :       1
 }
 
 const anim = {
-    position      :     0,
-    fps           :     7,
-    id            :     null,
-    timer         :     null
+    position        :       0,
+    fps             :       7,
+    id              :       null,
+    timer           :       null
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     zombie.domObj = document.querySelector("#zombietomb");
-    walkingDead();
+    //walkingDead();
+});
+
+document.addEventListener("keydown", event => {
+    if (zombie.direction === 1) {
+        if (event.code == "ArrowUp") zombie.domObj.style.transform += "translateY(-10px)";
+        if (event.code == "ArrowDown") zombie.domObj.style.transform += "translateY(10px)";
+        if (event.code == "ArrowLeft") zombie.domObj.style.transform += "translateX(-10px)";
+        if (event.code == "ArrowRight") zombie.domObj.style.transform += "translateX(10px)";
+        if (event.code == "Space") deadWalking();
+        if (event.code == "AltLeft") walkingDead();
+        if (event.code == "KeyR") zombie.domObj.style.transform += "scaleX(-1)", zombie.direction = 0;
+    } else if (zombie.direction === 0) {
+        if (event.code == "ArrowUp") zombie.domObj.style.transform += "translateY(-10px)";
+        if (event.code == "ArrowDown") zombie.domObj.style.transform += "translateY(10px)";
+        if (event.code == "ArrowLeft") zombie.domObj.style.transform += "translateX(10px)";
+        if (event.code == "ArrowRight") zombie.domObj.style.transform += "translateX(-10px)";
+        if (event.code == "Space") deadWalking();
+        if (event.code == "AltLeft") walkingDead();
+        if (event.code == "KeyR") zombie.domObj.style.transform += "scaleX(-1)", zombie.direction = 1;
+    }
 });
 
 const walkingDead = () => {
@@ -29,3 +50,15 @@ const walkingDead = () => {
         cancelAnimationFrame(anim.id);
     }
 };
+
+const deadWalking = () => {
+    anim.timer = setTimeout(()=> {anim.id = requestAnimationFrame(deadWalking)}, 1000/anim.fps);
+    zombie.domObj.style.background = `url(${zombie.image}) ${-zombie.spriteSize * anim.position}px 0px`;
+    anim.position --;
+    if (anim.position <= 0) {
+        anim.position = 0;
+        clearTimeout(anim.timer);
+        cancelAnimationFrame(anim.id);
+        zombie.domObj.style.background = null;
+    }
+}
